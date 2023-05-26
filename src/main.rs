2754d030f738fec1a0c1aa361bad94fb9bdd4ca0
vstalivyshel <112723363+vstalivyshel@users.noreply.github.com>
2023-05-26 14:27:25 +0300

    Jump instruction added. Basically infinite loop implemented. But now i need a way to break this loop.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod utils;
 
 const VM_STACK_CAPACITY: usize = 1024;
+const PROGRAM_INST_CAPACITY: usize = 1024;
 
 #[derive(Copy, Clone, Debug)]
 pub enum Panic {
     StackOverflow,
     StackUnderflow,
-    IlligalInstruction {
+    InstLimitkOverflow(usize),
+    EofReached,
+    DivByZero,
+    IncompatibleValue {
         inst: Instruction,
         val_a: Value,
         val_b: Value,
@@ -16,6 +20,8 @@ pub enum Panic {
 #[derive(Copy, Clone, Debug)]
 pub enum Instruction {
     Push(Value),
+    Jump(usize),
+    Eof,
     Sub,
     Mul,
     Div,
@@ -29,45 +35,81 @@ pub enum Value {
 }
 
 pub struct VM {
-    pub stack: [Value; VM_STACK_CAPACITY],
-    pub stack_size: usize,
-    pub debug: bool,
+    stack: [Value; VM_STACK_CAPACITY],
+    stack_size: usize,
+    program: [Instruction; PROGRAM_INST_CAPACITY],
+    inst_len: usize,
+    inst_ptr: usize,
+    debug: Dbg,
 }
+
+pub struct Dbg {
+    stack: bool,
+    inst: bool,
+}
+
+impl Dbg {
+    fn full() -> Self {
+        Self { stack: true, inst: true }
+    }
+
+    fn off() -> Self {
+        Self { stack: false, inst: false }
+    }
+}
+
 
 impl VM {
     fn init() -> Self {
         Self {
             stack: [Value::Null; VM_STACK_CAPACITY],
+            program: [Instruction::Eof; PROGRAM_INST_CAPACITY],
             stack_size: 0,
-            debug: false,
+            inst_len: 0,
+            inst_ptr: 0,
+            debug: Dbg::off(),
         }
     }
 
-    fn load_from_memmory(
-        &mut self,
-        program: &[Instruction],
-    ) -> Result<(), Panic> {
-        for inst in program.into_iter() {
-            if self.debug {
-                println!("{}", self);
-                println!("ВИКОНУЮ ІНСТРУКЦІЮ {}", inst);
-            }
-            self.execute_instruction(*inst)?;
+    fn load_from_memmory(&mut self, program: &[Instruction]) -> Result<(), Panic> {
+		let prog_len = program.len();
+
+        if prog_len > PROGRAM_INST_CAPACITY {
+            return Err(Panic::InstLimitkOverflow(prog_len));
+        }
+
+        self.inst_len = prog_len;
+        self.inst_ptr = prog_len;
+
+        for (i, inst) in program.iter().enumerate() {
+            self.program[i] = *inst;
         }
 
         Ok(())
     }
 
-    fn execute_instruction(&mut self, inst: Instruction) -> Result<(), Panic> {
-        use Instruction::*;
+    fn execute(&mut self) -> Result<(), Panic> {
+        while self.inst_ptr != 0 {
+            self.execute_instruction()?;
+            self.inst_ptr -= 1;
+        }
 
+        if self.debug.stack {
+            println!( "{}", self );
+        }
+
+        Ok(())
+    }
+
+    fn execute_instruction(&mut self) -> Result<(), Panic> {
+        use Instruction::*;
         fn op<F>(vm: &mut VM, f: F) -> Result<(), Panic>
         where
-            F: Fn(isize, isize) -> Result<isize, Panic>
+            F: Fn(isize, isize) -> Result<isize, Panic>,
         {
             match (vm.stack_pop()?, vm.stack_pop()?) {
                 (Value::Number(a), Value::Number(b)) => vm.stack_push(Value::Number(f(a, b)?)),
-                (val_a, val_b) => Err(Panic::IlligalInstruction {
+                (val_a, val_b) => Err(Panic::IncompatibleValue {
                     inst: Sum,
                     val_a,
                     val_b,
@@ -75,12 +117,36 @@ impl VM {
             }
         }
 
-        match inst {
+        if self.debug.stack {
+            println!( "{}", self );
+        }
+
+        let current_inst = self.program[self.inst_len - self.inst_ptr];
+
+        if self.debug.inst {
+            println!( "[ІНСТ({ip})] {current_inst}", ip = self.inst_ptr - 1);
+        }
+
+        match  current_inst {
+            Eof => Err(Panic::EofReached),
             Push(value) => self.stack_push(value),
-            Sum => op(self, |a,  b| Ok(a + b)),
-            Sub => op(self, |a,  b| Ok(a - b)),
-            Mul => op(self, |a,  b| Ok(a * b)),
-            Div => op(self, |a,  b| Ok(a / b)),
+            Jump(addr) => {
+                if addr > self.inst_len {
+                    return Err(Panic::StackUnderflow);
+                }
+                self.inst_ptr += addr;
+                self.execute_instruction()
+            }
+            Sum => op(self, |a, b| Ok(b + a)),
+            Sub => op(self, |a, b| Ok(b - a)),
+            Mul => op(self, |a, b| Ok(b * a)),
+            Div => op(self, |a, b| {
+                if a == 0 {
+                    Err(Panic::DivByZero)
+                } else {
+                    Ok(b / a)
+                }
+            }),
         }
     }
 
@@ -119,10 +185,30 @@ fn main() {
         Sum,
         Push(Number(-1)),
         Sub,
+        Push(Number(-1)),
+        Mul,
+        Div,
+        Push(Number(46)),
+        Sum,
+        Push(Number(10)),
+        Push(Number(1)),
+        Sum,
+        // Current instruction is 0
+        Jump(1),
     ];
 
-    let mut vm = VM::init();
-    vm.debug = true;
-    let _ = vm.load_from_memmory(&program);
-    println!("{}", vm);
+    let mut state = VM::init();
+
+    state.debug = Dbg::full();
+
+    if let Err(vm_err) = state.load_from_memmory(&program) {
+        eprintln!("[!] ПАНІКА: {vm_err}");
+    }
+
+    if let Err(vm_err) = state.execute() {
+        match vm_err {
+            Panic::EofReached => {}
+            err => eprintln!("[!] ПАНІКА: {err}"),
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,13 @@
-use crate::{Instruction, Panic, Value, VM, VM_STACK_CAPACITY};
+use crate::{Instruction, Panic, Value, PROGRAM_INST_CAPACITY, VM};
 use std::{error, fmt};
 
 impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Instruction::*;
         match self {
-            Push(v) => write!(f, "Стак-Доповнення: {v}"),
+            Push(v) => write!(f, "Стак-Доповнення({v})"),
+            Jump(i) => write!(f, "Повтор-Інструкції({i})"),
+            Eof => write!(f, "Кінцева-Інструкція"),
             Sum => write!(f, "Сумма"),
             Sub => write!(f, "Віднімання"),
             Mul => write!(f, "Множення"),
@@ -19,10 +21,16 @@ impl fmt::Display for Panic {
         use Panic::*;
         match self {
             StackOverflow => write!(f, "Стак переповнений"),
-            StackUnderflow => write!(f, "Спроба дістат неіснуюче значення зі стаку"),
-            IlligalInstruction { inst, val_a, val_b } => write!(
+            StackUnderflow => write!(f, "Спроба дістати неіснуюче значення зі стаку"),
+            EofReached => write!(f, "Досягнено кінцевої інструкції"),
+            InstLimitkOverflow(inst_size) => write!(
                 f,
-                "Нелегальна інструкція: {inst} для значеннь {val_a} та {val_b}"
+                "Перевищено ліміт інструкцій: {inst_size} із доступних {PROGRAM_INST_CAPACITY}"
+            ),
+            DivByZero => write!(f, "Спроба ділення на нуль"),
+            IncompatibleValue { inst, val_a, val_b } => write!(
+                f,
+                "Неможливо виконати інструкцію {inst} для значень {val_a} та {val_b}"
             ),
         }
     }
@@ -35,7 +43,7 @@ impl fmt::Display for Value {
         use Value::*;
         match self {
             Number(n) => write!(f, "Число({n})"),
-            Null => write!(f, "Нічого"),
+            Null => write!(f, "<Порожньо>"),
         }
     }
 }
@@ -44,14 +52,13 @@ impl fmt::Display for VM {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "СТАК: ВМІСТИМІСТЬ: {capa}; ПОТОЧНИЙ-РОЗМІР: {size};  ВЕРХНЄ ЗНАЧЕННЯ: \"{value}\"",
-            capa = VM_STACK_CAPACITY - self.stack_size,
-            size = self.stack_size,
+            "[СТАК({capa})] {value}  ",
             value = if self.stack_size != 0 {
                 self.stack[self.stack_size - 1]
             } else {
                 self.stack[self.stack_size]
-            }
+            },
+            capa =self.stack_size,
         )
     }
 }
